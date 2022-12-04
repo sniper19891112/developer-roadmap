e8b1ac14de7db3713edbfa7cd992b5a0b4f7753a
unknown <sniper19891112>
2022-12-04 21:58:28 +0900

    ...update

---
+++ pages/index.tsx
@@ -0,0 +1,158 @@
+import { Box, Container, Heading, SimpleGrid, Tag, Text } from '@chakra-ui/react';
+import { GlobalHeader } from '../components/global-header';
+import { Footer } from '../components/footer';
+import { OpensourceBanner } from '../components/opensource-banner';
+import { DimmedMore } from '../components/dimmed-more';
+import { LinksListItem } from '../components/links-list-item';
+import { VideoIcon } from '../components/icons/video-icon';
+import { LinksList } from '../components/links-list';
+import { HomeRoadmapItem } from '../components/roadmap/home-roadmap-item';
+import { getFeaturedRoadmaps, RoadmapType } from '../lib/roadmap';
+import { getAllGuides, GuideType } from '../lib/guide';
+import { getAllVideos, VideoType } from '../lib/video';
+import Helmet from '../components/helmet';
+import { PageWrapper } from '../components/page-wrapper';
+
+type HomeProps = {
+  roadmaps: RoadmapType[];
+  guides: GuideType[];
+  videos: VideoType[];
+};
+
+export default function Home(props: HomeProps) {
+  const { roadmaps, guides, videos } = props;
+
+  return (
+    <PageWrapper>
+      <GlobalHeader variant={'transparent'} />
+      <Helmet title="Developer Roadmaps" />
+      <Box>
+        <Container maxW="container.md" pb="90px">
+          <Box py={['23px', '23px', '35px']} color="gray.200">
+            <Heading
+              color="gray.50"
+              fontSize={['22px', '22px', '28px']}
+              mb={['8px', '8px', '15px']}
+            >
+              Hey there! 👋
+            </Heading>
+            <Text fontSize={['14px', '14px', '16px']} mb="10px">
+              <Text fontWeight={500} as="span">
+                roadmap.sh
+              </Text>{' '}
+              is a community effort to create roadmaps, guides and other
+              educational content to help guide the developers in picking up the
+              path and guide their learnings.
+            </Text>
+          </Box>
+
+          <Tag bg='gray.400' mb={4}>Role Based Roadmaps</Tag>
+          <SimpleGrid columns={[1, 2, 3]} spacing={['10px', '10px', '15px']} mb='40px'>
+            {roadmaps.filter(roadmap => roadmap.type === 'role').map((roadmap: RoadmapType, counter: number) => (
+              <HomeRoadmapItem
+                isUpcoming={roadmap.isUpcoming}
+                url={`/${roadmap.id}`}
+                key={roadmap.id}
+                colorIndex={counter}
+                title={roadmap.featuredTitle}
+                isCommunity={roadmap.isCommunity}
+                subtitle={roadmap.featuredDescription}
+              />
+            ))}
+          </SimpleGrid>
+
+          <Tag bg='gray.400' mb={4}>Tool Based Skill Trees</Tag>
+
+          <SimpleGrid columns={[1, 2, 3]} spacing={['10px', '10px', '15px']} mb='30px'>
+            {roadmaps.filter(roadmap => roadmap.type === 'tool').map((roadmap: RoadmapType, counter: number) => (
+              <HomeRoadmapItem
+                isUpcoming={roadmap.isUpcoming}
+                url={`/${roadmap.id}`}
+                key={roadmap.id}
+                colorIndex={counter}
+                title={roadmap.featuredTitle}
+                isCommunity={roadmap.isCommunity}
+                subtitle={roadmap.featuredDescription}
+              />
+            ))}
+          </SimpleGrid>
+        </Container>
+      </Box>
+
+      <Box bg="white">
+        <Container maxW="container.md">
+          <Box pt="60px" mb={['10px', '15px', '20px']}>
+            <Heading
+              color="green.500"
+              fontSize={['20px', '20px', '25px']}
+              mb="5px"
+            >
+              Video Explanations
+            </Heading>
+          </Box>
+
+          <LinksList>
+            {videos.map((video) => (
+              <LinksListItem
+                target={'_blank'}
+                key={video.id}
+                href={video.youtubeLink!}
+                badgeText={video.isPro ? 'PRO' : ''}
+                hideSubtitleOnMobile
+                title={video.title}
+                subtitle={video.duration}
+                icon={
+                  <VideoIcon
+                    style={{
+                      marginRight: '7px',
+                      width: '18px',
+                      height: '18px',
+                      color: '#9c9c9c',
+                    }}
+                  />
+                }
+              />
+            ))}
+            <DimmedMore href="/watch" text={'View all Videos'} />
+          </LinksList>
+        </Container>
+      </Box>
+
+      <Box pb="80px" bg="white">
+        <Container maxW="container.md" position="relative">
+          <Box pt="40px" mb="20px">
+            <Heading color="green.500" fontSize="25px" mb="5px">
+              Visual Guides
+            </Heading>
+          </Box>
+
+          <LinksList>
+            {guides.map((guide) => (
+              <LinksListItem
+                key={guide.id}
+                href={`/guides/${guide.id}`}
+                title={guide.title}
+                badgeText={guide.isPro ? 'PRO' : ''}
+                subtitle={guide.formattedUpdatedAt!}
+              />
+            ))}
+            <DimmedMore href={'/guides'} text="View all Guides" />
+          </LinksList>
+        </Container>
+      </Box>
+
+      <OpensourceBanner />
+      <Footer />
+    </PageWrapper>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      roadmaps: getFeaturedRoadmaps(),
+      guides: getAllGuides(10),
+      videos: getAllVideos(10),
+    },
+  };
+}
---
+++ readme.md
@@ -0,0 +1,72 @@
+<p align="center">
+  <img src="public/brand.png" height="128">
+  <h2 align="center">roadmap.sh</h2>
+  <p align="center">Community driven roadmaps, articles and resources for developers<p>
+  <p align="center">
+    <a href="https://roadmap.sh/roadmaps">
+    	<img src="https://img.shields.io/badge/-Roadmaps%20-0a0a0a.svg?style=flat&colorA=0a0a0a" alt="roadmaps" />
+    </a>
+    <a href="https://youtube.com/theroadmap?sub_confirmation=1">
+    	<img src="https://img.shields.io/badge/-Videos-0a0a0a.svg?style=flat&colorA=0a0a0a" alt="videos" />
+    </a>
+    <a href="https://github.com/kamranahmedse/developer-roadmap/tree/0471d44c8fae58b6a36a7c57bba12253916d0249/translations">
+    	<img src="https://img.shields.io/badge/-Translations-0a0a0a.svg?style=flat&colorA=0a0a0a" alt="videos" />
+    </a>
+    <a href="https://www.youtube.com/channel/UCA0H2KIWgWTwpTFjSxp0now?sub_confirmation=1">
+    	<img src="https://img.shields.io/badge/%E2%9D%A4-YouTube%20Channel-0a0a0a.svg?style=flat&colorA=0a0a0a" alt="roadmaps" />
+    </a>
+  </p>
+</p>
+
+<br>
+
+![](https://i.imgur.com/waxVImv.png)
+
+Roadmaps are being made interactive and have been moved to website.
+
+### [View all Roadmaps](https://roadmap.sh)
+
+![](https://i.imgur.com/waxVImv.png)
+
+Here is the list of available roadmaps with more being actively worked upon.
+
+- [Frontend Roadmap](https://roadmap.sh/frontend)
+- [Backend Roadmap](https://roadmap.sh/backend)
+- [DevOps Roadmap](https://roadmap.sh/devops)
+- [React Roadmap](https://roadmap.sh/react)
+- [Vue Roadmap](https://roadmap.sh/vue)
+- [Angular Roadmap](https://roadmap.sh/angular)
+- [Android Roadmap](https://roadmap.sh/android)
+- [Python Roadmap](https://roadmap.sh/python)
+- [Go Roadmap](https://roadmap.sh/golang)
+- [Java Roadmap](https://roadmap.sh/java)
+- [DBA Roadmap](https://roadmap.sh/postgresql-dba)
+
+![](https://i.imgur.com/waxVImv.png)
+
+If you think that these can be improved in any way, please do suggest. Also, if you would like to contribute to existing roadmaps or add a new roadmap, please open an issue or reach out to [@kamranahmedse](https://twitter.com/kamranahmedse).
+
+## Development
+
+Clone the repository, install the dependencies and start the application
+
+```bash
+git clone git@github.com:kamranahmedse/developer-roadmap.git
+npm install
+npm run dev
+```
+
+## Contribution
+
+> Have a look at [contribution docs](./contributing) for how to update any of the roadmaps
+
+- Suggest changes to existing roadmaps
+- Improve the site's codebase
+- Add new Roadmap
+- Write tests
+- Discuss ideas in issues
+- Spread the word
+
+## License
+
+Have a look at the [license file](./license) for details
